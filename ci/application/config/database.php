b44c1b3133af16c14b58fd268d9b6f2ca8161e42
abhinandSDU <114275064+abhinandSDU@users.noreply.github.com>
2022-12-31 14:59:37 +0530

    Update database.php

--- ci/application/config/database.php
+++ ci/application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => '52.87.165.113',
+	'username' => 'newuser',
+	'password' => 'password',
 	'database' => 'amrithaahead',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',

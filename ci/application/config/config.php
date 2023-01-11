db3b736736b3744abc588f23674aa702baa6b7da
abhinandSDU <114275064+abhinandSDU@users.noreply.github.com>
2023-01-11 10:46:34 +0530

    Update config.php

--- ci/application/config/config.php
+++ ci/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
  */
-$config['base_url'] = '18.212.41.119/ahead';
+$config['base_url'] ='18.212.41.119/ahead';
 
 /*
 |--------------------------------------------------------------------------
